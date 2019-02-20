760fcba22b2b7ef53481f245ce06db10f4325e27
Aster <galaster@foxmail.com>
2019-02-20 20:20:38 +0800

    🎂 Project initialized!

---
+++ Kernel/init.m
@@ -0,0 +1,3 @@
+(* ::Package:: *)
+
+Get["CatchWaifu`CatchWaifu`"];
---
+++ PacletInfo.m
@@ -0,0 +1,9 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+Paclet[
+	Name -> "CatchWaifu",
+	Version -> "1.0.0",
+	MathematicaVersion -> "11.1+"
+]
